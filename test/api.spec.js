29366eab7845c577590e7cba8b6e3c4162192fae
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-07-10 11:57:06 -0600

    Adds happy and sad path testing to /api/v1/recipes/food_search

--- test/api.spec.js
+++ test/api.spec.js
@@ -2,6 +2,7 @@ var shell = require('shelljs');
 var request = require("supertest");
 var app = require('../app');
 var {sequelize} = require('../models');
+var recipe1, recipe4
 
 var Recipe = require('../models').Recipe;
 
@@ -10,6 +11,22 @@ describe('api', () => {
   beforeAll(async () => {
     await shell.exec('npx sequelize db:create', {silent: true});
     await shell.exec('npx sequelize db:migrate', {silent: true});
+    for( let i = 1; i <= 6; i++){
+      let recipe = await Recipe.create({
+        url: `www.example${i}.com`,
+        label: `Calories${i*100}`,
+        cal_per_serving: i*100,
+        num_of_ingredients: i,
+        baseFood: 'Food1'
+      })
+
+      if( i === 1){
+        recipe1 = recipe
+      }
+      if( i === 4){
+        recipe4 = recipe
+      }
+    }
   });
 
   afterAll(async() => {
@@ -19,21 +36,11 @@ describe('api', () => {
     await sequelize.close();
   });
 
+  // beforeAll( async() => {
+  //
+  // });
   describe('Test Recipe Paths', () => {
     describe('GET api/v1/recipes/calorie_search?q=calorie_count', () => {
-      beforeAll( async() => {
-        for( let i = 1; i <= 6; i++){
-          let recipe = await Recipe.create({
-            url: `www.example${i}.com`,
-            label: `Calories${i*100}`,
-            cal_per_serving: i*100
-          })
-
-          if( i === 1){
-            var recipe1 = recipe
-          }
-        }
-      });
 
       test('failure; inappropriate query string', async () => {
         return request(app)
@@ -75,9 +82,42 @@ describe('api', () => {
 
     });
 
-    test('GET api/v1/recipes/food_search?q=food_type', async () => {
+    describe('Test food_type path', () => {
+      test('GET api/v1/recipes/food_search?q=food_type', async () => {
+        return request(app)
+        .get('/api/v1/recipes/food_search?q=food1')
+        .then(async function(response){
+          expect(response.statusCode).toBe(200);
+          expect(response.body).toHaveProperty("data")
+          expect(response.body.data).toHaveProperty("recipes");
 
-    });
+          let returnedRecipes = response.body.data.recipes;
+          expect(returnedRecipes).toHaveLength(6)
+          for( let returnedRecipe of returnedRecipes){
+            expect(returnedRecipe).toHaveProperty('id');
+            expect(returnedRecipe).toHaveProperty('label');
+            expect(returnedRecipe).toHaveProperty('url');
+            expect(returnedRecipe).toHaveProperty('cal_per_serving');
+
+            if( returnedRecipe.label === "Calories100" ){
+              expect(returnedRecipe).toHaveProperty('url', 'www.example1.com');
+              expect(returnedRecipe).toHaveProperty('cal_per_serving', 100);
+              expect(returnedRecipe).toHaveProperty('id', recipe1.id);
+            }
+          }
+        })
+      });
+
+      test('failure; inappropriate query string', async () => {
+        return request(app)
+                .get('/api/v1/recipes/food_search?q=NonExistentFood')
+                .then(async function(response){
+                  expect(response.statusCode).toBe(404);
+                  expect(response.body).toHaveProperty("error", "Food not found.");
+                })
+      });
+
+    })
 
     test('GET api/v1/recipes/ingredient_search?q=num_of_ingredients', async () => {
 
